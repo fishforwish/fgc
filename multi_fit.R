8a847d99970654d4bf4bfd8ab03817bd4ff73df7
mli2830 <wzmichael.li@gmail.com>
2016-03-09 00:12:00 -0500

    baseline is casuing a problem

--- multi_fit.R
+++ multi_fit.R
@@ -4,15 +4,14 @@ library(reshape2)
 library(ordinal)
 
 
-dat <- data.frame(id= 1:nrow(combinedDat),combinedDat)
+dat <- data.frame(obs= 1:nrow(combinedDat),combinedDat)
 
-aa <- melt(dat,id=c("id","fgc","beneScore","clusterId"))
+aa <- melt(dat,id=c("obs","fgc","beneScore","clusterId"))
 
-dat2 <- aa %>% mutate(id = as.factor(id),
-                      value = as.factor(value))
+dat2 <- aa %>% mutate(value = as.factor(value))
 
 str(dat2)
 
-fullmod <- clmm(value~variable+fgc+beneScore+(1|clusterId)+(1|clusterId:id),
+fullmod <- clmm(value~variable+fgc+beneScore+ (0+variable|clusterId),
                data = dat2)
 
