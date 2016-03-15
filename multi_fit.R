b8e0277b0199b5b5b86a563b3d38bfcca7344e77
mli2830 <wzmichael.li@gmail.com>
2016-03-15 13:36:55 -0400

    took out PC scores out of df

--- multi_fit.R
+++ multi_fit.R
@@ -3,6 +3,8 @@ library(tidyr)
 library(reshape2)
 library(ordinal)
 
+combinedDat <- combinedDat %>% select(c(continueFgc,daughterToFgc,fgc,
+                                        beneScore,clusterId))
 
 combinedDatID <- data.frame(id= 1:nrow(combinedDat),combinedDat)
 
@@ -27,8 +29,6 @@ fullmod2 <- clmm(value~futurefgc+fgc+beneScore + (0+futurefgc|clusterId),
                 data = mcombinedDatID)
 
 summary(fullmod2)
-
-
 
 summary(fullmod2)
 
