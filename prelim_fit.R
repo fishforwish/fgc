2b77f23266424d6f1e30fe6dbda0732efac7642e
mli2830 <wzmichael.li@gmail.com>
2016-03-15 13:31:13 -0400

    pushing logistic PCA

--- prelim_fit.R
+++ prelim_fit.R
@@ -1,4 +1,5 @@
 library(ordinal)
+library(logisticPCA)
 library(dplyr)
 ##Response: 
 ## Y1 = overall FGC future (continueFgc) 
@@ -33,6 +34,14 @@ beneNames <- grepl("bene", names(df))
 fgcDat <- df %>% select(c(continueFgc,daughterToFgc,fgc))
 covsDat <- df %>% select(clusterId)
 beneDat <- df[beneNames] 
+beneDat2 <- beneDat %>% rowwise() %>% summarise_each(funs(numchange))
+
+logPCA <- logisticPCA(beneDat2,k=1,m=6)
+
+logPCA2 <- logisticPCA(beneDat2,k=2,m=6)
+PCAscores2 <- logPCA2$PCs
+
+
 
 fgcDat <- (fgcDat
 	%>% rowwise() %>% 
@@ -46,13 +55,20 @@ beneDat <- beneDat %>% rowwise() %>% summarise_each(funs(numchange))
 
 belief_score <- factanal(beneDat,factors=1,scores="regression")$score
 
-combinedDat <- data.frame(fgcDat, beneScore=belief_score[,1],covsDat)
+combinedDat <- data.frame(fgcDat, beneScore=belief_score[,1],covsDat, 
+                          P1=logPCA$PCs,
+                          PC1 = PCAscores2[,1],PC2 = PCAscores2[,2])
 
-daughtermod <- clmm2(daughterToFgc~fgc+beneScore,
-                     random = clusterId,
-                     Hess = TRUE,
+daughtermod <- clmm(daughterToFgc~fgc+beneScore+ (1|clusterId),
                      data=combinedDat)
 summary(daughtermod)
+
+daughtermodP1 <- update(daughtermod,.~fgc+P1+(1|clusterId))
+summary(daughtermodP1)
+
+daughtermodPC <- update(daughtermod,.~fgc+PC1+PC2+(1|clusterId))
+summary(daughtermodPC)
+
 
 contmod <- clmm2(continueFgc~fgc+beneScore,
                  random = clusterId,
