73b1293180240ef103d96b5cf1c9c06031e00407
Your Name <you@example.com>
2016-03-15 14:24:46 -0400

    Meeting with Mike and Chyun

--- multi_fit.R
+++ multi_fit.R
@@ -3,32 +3,29 @@ library(tidyr)
 library(reshape2)
 library(ordinal)
 
-combinedDat <- combinedDat %>% select(c(continueFgc,daughterToFgc,fgc,
-                                        beneScore,clusterId))
+combinedDat <- combinedDat %>% select(
+	c(continueFgc,daughterToFgc,fgc, beneScore,clusterId)
+)
 
 combinedDatID <- data.frame(id= 1:nrow(combinedDat),combinedDat)
 
 mcombinedDatID <- melt(combinedDatID,id=c("id","fgc","beneScore","clusterId"))
 
 mcombinedDatID <- (mcombinedDatID %>% 
-  mutate(value = as.factor(value)
-         , futurefgc = variable
-#         , id = as.factor(id)
+  mutate(futurefgc = as.factor(value)
+         , respType = variable
+			, fgcStatusMom = fgc
   )
-    %>% select(-variable)
+    %>% select(-c(variable, value, fgc))
 )
 
 str(mcombinedDatID)
 
-fullmod <- clmm(value~fgc+beneScore + (0+futurefgc|clusterId),
-                  data = mcombinedDatID)
+fullmod <- clmm(futurefgc~
+		respType*(fgcStatusMom+beneScore)
+		+ (0+respType|clusterId)
+	, data = mcombinedDatID
+)
 
 summary(fullmod)
-
-fullmod2 <- clmm(value~futurefgc+fgc+beneScore + (0+futurefgc|clusterId),
-                data = mcombinedDatID)
-
-summary(fullmod2)
-
-summary(fullmod2)
 
