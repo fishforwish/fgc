3613f79a62f891759a369b7b52de4216b5736986
mli2830 <wzmichael.li@gmail.com>
2016-03-17 15:56:47 -0400

    task updated

--- mcmcglmm_fit.R
+++ mcmcglmm_fit.R
@@ -3,7 +3,7 @@ library(MCMCglmm)
 combinedDatID <- data.frame(id=1:(nrow(combinedDat)),combinedDat)
 
 prior.c <- list(R=list(list(V=diag(2),n=2)),
-                G=list(list(V=diag(2)/3,n=2)))
+                G=list(list(V=diag(2),n=2)))
 
 MCMCmod <- MCMCglmm(cbind(as.factor(continueFgc),as.factor(daughterToFgc))~trait-1+fgc+beneScore,
                 random=~us(trait):clusterId,
