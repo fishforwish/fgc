22f46bf86d9be5b00290b334feaf0d25bb6ca4ac
mli2830 <wzmichael.li@gmail.com>
2016-03-17 15:03:11 -0400

    bad mixing, back to simulations

--- mcmcglmm_fit.R
+++ mcmcglmm_fit.R
@@ -2,10 +2,16 @@ library(MCMCglmm)
 
 combinedDatID <- data.frame(id=1:(nrow(combinedDat)),combinedDat)
 
+prior.c <- list(R=list(list(V=diag(2),n=2)),
+                G=list(list(V=diag(2)/3,n=2)))
+
 MCMCmod <- MCMCglmm(cbind(as.factor(continueFgc),as.factor(daughterToFgc))~trait-1+fgc+beneScore,
                 random=~us(trait):clusterId,
                 rcov=~us(trait):id,
+                prior=prior.c,
                 data=combinedDatID,
                 family=c("ordinal","ordinal"))
+
+
 
 summary(MCMCmod)
