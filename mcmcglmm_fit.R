4dd86de87123ca06d69639ea3b9adb0a3a9b5266
Your Name <you@example.com>
2016-04-06 17:26:32 -0400

    Autocommit (fgc) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   mcmcglmm_fit.R
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	.localdir/
    	KE.df.RDS
    	ML.df.RDS
    	NG.df.RDS
    	SL.df.RDS
    	commit.time
    	ke5.df.Rds
    	localdir/
    	ml5.df.Rds
    	ng5.df.Rds
    	sl5.df.Rds

--- mcmcglmm_fit.R
+++ mcmcglmm_fit.R
@@ -1,5 +1,5 @@
 library(MCMCglmm)
-
+nitt <- 50000
 for (r in grep("Rds$", input_files, value=TRUE)){
 	if (exists("dat"))
 		dat <- rbind(dat, readRDS(r))
@@ -14,18 +14,18 @@ prior.c <- list(R=list(list(V=diag(2),nu=5)),
 print(summary(dat))
 
 MCMCmod <- MCMCglmm(
-	cbind(as.factor(futurefgc),as.factor(futurefgcDau)) ~ trait
-		+ fgcstatusMom
+	cbind(as.factor(futurefgc),as.factor(futurefgcDau)) ~ trait * (
+		fgcstatusMom
 		+ bene + media + att
 		+ group_bene + group_media + group_att + group_fgc
 	  	+ splines::ns(age, 4) + splines::ns(wealth, 4) 
 		+ edu + maritalStat + job + urRural + CC
 		+ ethni + religion
-		- 1
+		) - 1
 	, random=~us(trait):clusterId
 	, rcov=~us(trait):units
 	, prior=prior.c
-	, nitt = 50000
+	, nitt = nitt
 	, data=dat
 	, verbose=FALSE
 	, family=c("ordinal","ordinal")
