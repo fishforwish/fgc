b03db16abea39f41111b72a41124520e02643d4b
mli2830 <wzmichael.li@gmail.com>
2016-05-04 17:24:43 -0400

    still not clear with random effects

--- futurefgc_fit.R
+++ futurefgc_fit.R
@@ -6,12 +6,47 @@ for (r in grep("Rds$", input_files, value=TRUE)){
   else
     dat <- readRDS(r)
 }
-
+set.seed(101)
+dat <- dat[sample(1:nrow(dat),5000),]
 
 prior.c <- list(R=list(list(V=diag(1),nu=5)),
-                G=list(list(V=diag(1),nu=5)))
+                G=list(list(Vcomm=diag(1),nu=5)
+                       , list(Veth=diag(1),nu=5)
+                       , list(VCCB=diag(4),nu=5)
+                       , list(VCCA=diag(4),nu=5)
+                       , list(VCCM=diag(4),nu=5)
+#                        , list(VCCAGE=diag(1),nu=5)
+#                        , list(VCCW=diag(1),nu=5)
+))
 
 print(summary(dat))
+
+futurefgc_ind <- MCMCglmm(
+  as.factor(futurefgc) ~ fgcstatusMom
+    + CC
+    + bene + media + att + edu
+    + splines::ns(age, 4) + splines::ns(wealth, 4)
+    + maritalStat 
+    + job
+    + urRural + religion
+    - 1
+  , random=~clusterId + ethni 
+    + us(CC):bene + us(CC):att + us(CC):media 
+    # + CC:splines::ns(age,4) + CC:splines::ns(wealth,4) 
+  , rcov=~units
+  , prior=prior.c
+  , nitt = nitt
+  , data=dat
+#   , singular.ok = TRUE
+   , verbose=TRUE
+#   , slice = TRUE
+  , family="ordinal"
+)
+
+print(summary(futurefgc_ind))
+
+
+quit()
 # 
 # futurefgc_full <- MCMCglmm(
 #     as.factor(futurefgc) ~ CC:fgcstatusMom
@@ -34,23 +69,3 @@ print(summary(dat))
 # )
 # 
 # print(summary(futurefgc_full))
-dat2 <- head(dat,4000)
-futurefgc_ind <- MCMCglmm(
-  as.factor(futurefgc) ~ CC:fgcstatusMom
-    + CC:bene + CC:media + CC:att + CC:edu
-    + CC:splines::ns(age, 4) + CC:splines::ns(wealth, 4)
-    + CC:maritalStat 
-    + CC:job
-    + CC:urRural + CC:ethni + CC:religion
-  , random=~clusterId
-  , rcov=~units
-  , prior=prior.c
-  , nitt = nitt
-  , data=dat2
-  , singular.ok = TRUE
-  , verbose=TRUE
-  , slice = TRUE
-  , family="ordinal"
-)
-
-print(summary(futurefgc_ind))
