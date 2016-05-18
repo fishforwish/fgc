eb55b36dd2695e360206be2bfd9e48a4d07def31
mli2830 <wzmichael.li@gmail.com>
2016-05-18 13:19:33 -0400

    adding norm.R

--- norm.R
+++ norm.R
@@ -1,39 +1,44 @@
 library(dplyr)
 library(ggplot2)
+input_files <- c("all_countries_futurefgc", "ke5.futurefgc.Rds", "ng5.futurefgc.Rds", "sl5.futurefgc.Rds", "ml5.futurefgc.Rds")
+for (r in grep("Rds$", input_files, value=TRUE)){
+  if (exists("dat"))
+    dat <- rbind(dat, readRDS(r))
+  else
+    dat <- readRDS(r)
+}
 
-mod1 <- futurefgc_ind
-mod2 <- futurefgc_full
+load(".all.futurefgc.RData")
+
+predsd <- (dat
+  %>% ungroup()
+  %>% summarise_each(funs(sd))
+)
+
+ind_df <- data.frame(futurefgc_ind$Sol)
+full_df <- data.frame(futurefgc_full$Sol)
+
+adjust <- function(x,sddf,df){
+  tempvec <- df[,x]*t(sddf[,x])
+  return(tempvec)
+}
 
 q <- c(0.025,0.05,0.25,0.5,0.75,0.95,0.975)
 
-ind <- data.frame(mod1$Sol)
-ind <- (ind 
-  %>% select(c(fgcstatusMom
-    , bene
-    , media
-    , att
-    , edu)
-    )
+pred_ind <- c("fgcstatusMom","bene","att","media","edu")
+pred_full <- c("fgcstatusMom","group_fgcstatusMom"
+               , "bene", "group_bene" 
+               , "att", "group_att"
+               , "media", "group_media"
+               , "edu", "group_edu")
+pred_ind <- c("fgcstatusMom","bene","att","media","edu")
+adjust_ind <- (adjust(pred_ind,predsd,ind_df) 
+  %>% summarise_each(funs(mean))
 )
 
-indsum<-apply(ind2,2,function(v){quantile(v,q)})
-
-
-full <- data.frame(mod2$Sol)
-full <- (full 
-  %>% select(c(fgcstatusMom
-  , bene
-  , media
-  , att
-  , edu
-  , group_bene
-  , group_att
-  , group_media
-  , group_fgc
-  , group_edu)
-  )
+print(adjust_ind)
+adjust_full <- (adjust(pred_full,predsd,full_df) 
+  %>% summarise_each(funs(mean))
 )
 
-fullsum<-apply(full,2,function(v){quantile(v,q)})
-
-
+print(adjust_ind)
