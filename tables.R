be3579bc501c656bbc6a2a900a27c3ad2db62e6b
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-11-13 14:57:49 -0500

    modified:   tables.R

--- tables.R
+++ tables.R
@@ -8,15 +8,22 @@ library(dplyr)
 #   %>% select(-c(Age,baseAge,interviewYear, oldMC, MCcat,))
 # )
 # Answers <- Answers[complete.cases(Answers),]
-Ans <- (rdsRead()
+
+Ans <- (rdsRead())
+
+names(Ans)
+
+Ans <- (Ans
 	%>% select(Persist, fgcstatus, CC 
-  , age , Residence , Religion , MaritalStatus , Job , Education
-  , mediaNpmg , mediaRadio , mediaTv
-  , attNoTell , attNegKids , attArgue , attRefuseSex , attBurnFood
-  , heardFGC
-  , beneHygiene , beneAcceptance , beneMarriage , benePreventPreSex
-  , benePleasureM , beneReligion , beneRedPromis , beneRedSTD , beneOther
-  , daughterPlan  , daughterFgced 
+		, age, Residence, Religion, MaritalStatus, Job, Education
+		, mediaNpmg, mediaRadio, mediaTv
+		, attNoTell, attNegKids, attArgue, attRefuseSex, attBurnFood
+		, heardFGC
+		, beneHygiene, beneAcceptance, beneMarriage, benePreventPreSex
+		, benePleasureM, beneReligion
+		## , beneRedPromis , beneRedSTD
+		, beneOther
+		, daughterPlan , daughterFgced 
 	)
 	%>% mutate(AgeGroup = ifelse(between(age,14,19),"14-19"
 		, ifelse(between(age,20,24),"20-24"
@@ -33,7 +40,7 @@ Ans <- (rdsRead()
 summary(Ans)
 
 CCtable <- function(x){
-  return(table(Ans[[x]],Ans[["CC"]]))
+	return(table(Ans[[x]],Ans[["CC"]]))
 }
 
 predictors <- c("AgeGroup","Residence","Religion","Education","Job"
@@ -41,16 +48,16 @@ predictors <- c("AgeGroup","Residence","Religion","Education","Job"
 	, "heardFGC", "fgcstatus", "daughterPlan", "daughterFgced", "Persist"
 	, "mediaNpmg","mediaRadio","mediaTv" 
 	, "beneHygiene", "beneAcceptance", "beneMarriage", "benePreventPreSex"
-	, "benePleasureM", "beneReligion", "beneRedPromis", "beneRedSTD", "beneOther"
+	, "benePleasureM", "beneReligion", "beneOther"
 	, "attNoTell", "attNegKids", "attArgue", "attRefuseSex", "attBurnFood"
 )
 
 tablist <- lapply(predictors,CCtable)
 
 combdf <- function(pred,lldf){
-  tempdf <- as.data.frame.matrix(lldf)
-  df <- data.frame(Category=pred,Sub_Category=rownames(tempdf),tempdf)
-  return(df)
+	tempdf <- as.data.frame.matrix(lldf)
+	df <- data.frame(Category=pred,Sub_Category=rownames(tempdf),tempdf)
+	return(df)
 }
 
 
@@ -59,41 +66,41 @@ print(combdf)
 
 ddtab <- data.frame()
 for(i in seq_along(predictors)){
-  ddtab <- rbind(ddtab,combdf(predictors[i],tablist[[i]]))
+	ddtab <- rbind(ddtab,combdf(predictors[i],tablist[[i]]))
 }
 
 row.names(ddtab) <- NULL
 
 
 ddtab2 <- (ddtab 
-  %>% mutate(Total = KE+ML+SL+NG)
+	%>% mutate(Total = KE+ML+SL+NG)
 )
 
 
 print(ddtab2)
 
 ddtabpercent <- (ddtab2
-  %>% group_by(Category)
-  %>% mutate(Category2 = Sub_Category
-  , KE = round(KE*100/sum(KE),1)
-  , ML = round(ML*100/sum(ML),1)
-  , NG = round(NG*100/sum(NG),1)
-  , SL = round(SL*100/sum(SL),1)
-  )
-  %>% select(-c(Sub_Category))
+	%>% group_by(Category)
+	%>% mutate(Category2 = Sub_Category
+	, KE = round(KE*100/sum(KE),1)
+	, ML = round(ML*100/sum(ML),1)
+	, NG = round(NG*100/sum(NG),1)
+	, SL = round(SL*100/sum(SL),1)
+	)
+	%>% select(-c(Sub_Category))
 )
 
 
 ddtab3 <- (ddtab2
-  %>% select(-Sub_Category)
-  %>% group_by(Category)
-  %>% summarise_each(funs(sum)) 
-  %>% ungroup()
-  %>% mutate(Category2=as.factor("Total"))
-  %>% bind_rows(ddtabpercent,.)
-  %>% arrange(Category)
-  %>% ungroup()
-  %>% select(Category,Category2,KE:SL,Total)
+	%>% select(-Sub_Category)
+	%>% group_by(Category)
+	%>% summarise_each(funs(sum)) 
+	%>% ungroup()
+	%>% mutate(Category2=as.factor("Total"))
+	%>% bind_rows(ddtabpercent,.)
+	%>% arrange(Category)
+	%>% ungroup()
+	%>% select(Category,Category2,KE:SL,Total)
 )
 
 
